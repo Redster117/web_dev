cb46faaefd6c752ebed6a90a4eaaf90bdd4aec05
Jarred Joseph Cuschieri <jarred.cuschieri@education.nsw.gov.au>
2026-03-10 23:02:23 +0000

    continued coding from the last commit message:

--- web_app.py
+++ web_app.py
@@ -3,3 +3,8 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route("/", methods=["GET", "POST"])
+def upload_files():
+    
