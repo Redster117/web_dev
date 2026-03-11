72e06ed4af8e3425781ba6abf4a72cf5a1a6276d
Jarred Joseph Cuschieri <jarred.cuschieri@education.nsw.gov.au>
2026-03-11 23:55:47 +0000

    edited web app

--- web_app.py
+++ web_app.py
@@ -6,5 +6,15 @@ UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route("/", methods=["GET", "POST"])
-def upload_files():
-    
+def upload_file():
+    if request.method == "POST":
+        file = request.files["file"]
+        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(file_path)
+        with open(file_path, "r", encoding = "utf8") as f:
+            content = f.read
+        return f"<h3>File content:</h3><pre>{content}</pre>"
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
